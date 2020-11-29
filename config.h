087b5a8c59a2e5ede2523ddb40b75e85f17eb334
Tommy Stevenson <tommy@stevenson.us>
2020-11-29 10:43:22 -0600

    add: vanitygaps

--- config.h
+++ config.h
@@ -11,6 +11,11 @@ static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "togg
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+static unsigned int gappih    	    = 10;       /* horiz inner gap between windows */
+static unsigned int gappiv          = 10;       /* vert inner gap between windows */
+static unsigned int gappoh          = 10;       /* horiz outer gap between windows and screen edge */
+static unsigned int gappov          = 10;       /* vert outer gap between windows and screen edge */
+static int smartgaps                = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
